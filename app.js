e381b465d870b163e60bdbaa3e5602f6fd9340b8
majakulpa <majaweronikakulpa@gmail.com>
2019-07-15 17:24:24 +1000

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const compression = require("compression");
+const cors = require("cors");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
@@ -24,6 +25,10 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 // 1) GLOBAL MIDDLEWARES
+//implement CORS
+app.use(cors());
+
+app.options("*", cors());
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
 
